f6951b2e2b2c49ec67c7e7e86633fc71709bee50
Jesse <jessesmail.jve@gmail.com>
2018-09-23 21:03:18 +0200

    Random woord genereren af - opdracht 3

---
+++ Programmeren_praktijk/Opdracht3__/GalgjeSpel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht3__
+{
+    struct HangmanGame
+    {
+        public string secretWord;
+        public string guessedWord;
+
+        public void Init()
+        {   
+            for(int i = 0; i < secretWord.Length; i++)
+            {
+                guessedWord += ".";
+            }
+        }
+    }
+}
---
+++ Programmeren_praktijk/Opdracht3__/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht3__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HangmanGame hangman;
+
+            hangman.secretWord = "spoon";
+            hangman.guessedWord = "";
+            hangman.Init();
+            Console.WriteLine("The secret word is: " + hangman.secretWord);
+            Console.WriteLine("The guessed word is: " + hangman.guessedWord);
+
+            string word = SelectWord(WordList());
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+
+        static List<string> WordList()
+        {
+            List<string> x = new List<string>();
+            x.AddRange(new string[] {"Antiparticle", "Atom", "Duality", "Electron", "Cosmology", "Geodesic", "Mass", "Neutrino", "Neutron",
+                                     "Nucleus", "Photon", "Positron", "Proton", "Pulsar", "Quantum", "Quark", "Radar", "Radioactivity", "Singularity", "Spectrum"});
+
+            return x;
+        }
+        static string SelectWord(List<string> x)
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(x.Count);
+            string selectedWord = (string)x[r];
+
+            return selectedWord;
+        }
+    }
+}
